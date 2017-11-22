2bcd34d55a83c6e34efe197e2597a20149465bf8
ShawnAlchemize <1602444j@student.tp.edu.sg>
2017-11-22 14:13:08 +0800

    Add files via upload

---
+++ productpage/index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<title>My Website</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>My website</h1>
+			</div>
+			<div id="content">
+				<div id="nav">
+					<h3>Navigation</h3>
+					<ul>
+						<li><a class="selected" href="">Home</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</div>
+				
+				<div id="main">
+					<h2>Home Page</h2>
+					<p> LikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikeyLikey  </p>
+					<p> DoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeonDoyeon </p>
+				
+				</div>
+				<div id="footer">
+					Copyright &copy; 2017 Alchemize.
+				</div>
+			
+			</div>
